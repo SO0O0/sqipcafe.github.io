10898ec1d654021775fd18c92afc72cc410aa5ef
SouHirawata <hirawatasou@gmail.com>
2022-11-20 19:51:46 +0900

    :sparkles:Header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CafeHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import CafeHeader from "./components/CafeHeader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    CafeHeader
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@charset "UTF-8";
+
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+  vertical-align: baseline;
+  background: 0 0;
 }
 </style>
---
+++ src/components/CafeHeader.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="header_inner">
+        <div class="header_title">
+          <h1 class="header_logo">
+            <a href="#">
+              <img src="../assets/logo.png" alt="SQIP Cafe" />
+            </a>
+            <p class="time">新横浜のスペシャルティコーヒー専門店</p>
+          </h1>
+        </div>
+        <div>
+          <ul class="header_info">
+            <li>
+              <a href="#">アクセス</a>
+            </li>
+            <li>
+              <a href="#">サイトマップ</a>
+            </li>
+            <li class="iquiry">
+              <a href="#">お問い合わせ</a>
+            </li>
+          </ul>
+          <nav class="global_menu">
+            <ul>
+              <li>
+                <a href="#">HOME</a>
+              </li>
+              <li>
+                <a href="#">CONCEPT</a>
+              </li>
+              <li>
+                <a href="#">DRINK</a>
+              </li>
+              <li>
+                <a href="#">CAKE</a>
+              </li>
+              <li>
+                <a href="#">FOOD</a>
+              </li>
+              <li>
+                <a href="#">TAKEOUT</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <div class="hamburger">
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CafeHeader"
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: block;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  opacity: 1;
+
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+
+  h1 {
+    font-size: 12px;
+    display: flex;
+    height: auto;
+    align-items: center;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  .header_inner {
+    display: flex;
+    justify-content: space-between;
+
+    .header_info {
+      color: #fff;
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+      justify-content: flex-end;
+      font-weight: 700;
+      width: 380px;
+      border-radius: 0 0 10px 10px;
+      margin-bottom: 20px;
+      font-size: 13px;
+    }
+
+    li {
+      margin-left: 5%;
+    }
+
+    .iquiry {
+      background: #f4bb00;
+      padding: 15px 40px;
+
+      a {
+        color: #000;
+        font-weight: bold;
+      }
+    }
+    .global_menu {
+      ul {
+        display: flex;
+        justify-content: space-between;
+        font-size: 16px;
+
+        li {
+          margin-left: 40px;
+        }
+      }
+    }
+    .header_title {
+      text-align: center;
+      color: #fff;
+      font-weight: 700;
+      line-height: 150%;
+      margin-top: 20px;
+
+      .header-logo {
+        margin-bottom: 5px;
+
+        img {
+          line-height: 1;
+          font-size: 0;
+          height: auto;
+          max-width: 100%;
+          margin-right: 10px;
+          vertical-align: top;
+        }
+      }
+    }
+  }
+}
+</style>
